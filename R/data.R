551727686e822be478d546ffe9e82365e17d31f4
g_netto <giuliano.netto@biome-hub.com>
2022-01-01 21:45:27 -0800

    add docs

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Predictive Model Evaluation Data.
+#'
+#' A dataset containing the outcomes (0 or 1) and predictions (probabilities)
+#' for an example model. Based on `rmda::dcaData`.
+#'
+#' @format A data frame with 500 rows and 2 variables:
+#' \describe{
+#'   \item{outcomes}{binary outcomes (0 or 1, int.)}
+#'   \item{predictions}{predicted probabilities (num.)}
+#'   ...
+#' }
+#' @source \url{https://giulianonetto.github.io/bayesdca/}
+"PredModelData"
